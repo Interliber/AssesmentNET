e384410e41685f6cc4da2f6a303572038b0e54a6
BorjaLucero <brjlcr@gmail.com>
2022-11-04 13:31:33 +0100

    first commit

---
+++ AssesmentNET/Controllers/ClaimController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AssesmentNET.Models;
+
+namespace AssesmentNET.Controllers
+{
+    public class ClaimController : Controller
+    {
+        public IList<Claims> claimsList = new List<Claims>
+        {new Claims() {Id = 1, Date = "25/05/2022", Description = "This is a claim", Status = "Pending" },
+        new Claims() {Id = 1, Date = "25/05/2022", Description = "This is a claim", Status = "Pending" },
+        new Claims() {Id = 1, Date = "25/05/2022", Description = "This is a claim", Status = "Pending" },
+        new Claims() {Id = 1, Date = "25/05/2022", Description = "This is a claim", Status = "Pending" },
+        new Claims() {Id = 1, Date = "25/05/2022", Description = "This is a claim", Status = "Pending" }
+        };
+        // GET: ClaimController
+        public ActionResult Index()
+        {
+           
+            return View(claimsList.OrderBy(c => c.Id).ToList());
+        }
+
+        // GET: ClaimController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClaimController/Create
+        public ActionResult Create()
+        {
+            var claimNew = new Claims();
+            return View(claimNew);
+        }
+        public ActionResult CreateClaim(Claims claim)
+        {
+            claimsList.Add(claim);
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+
+        // GET: ClaimController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var cla = claimsList.Where(c => c.Id == id).FirstOrDefault();
+            return View(cla);
+        }
+
+    }
+}
---
+++ AssesmentNET/Controllers/VehiclesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AssesmentNET.Models;
+
+namespace AssesmentNET.Controllers
+{
+    public class VehiclesController : Controller
+    {
+
+        private IList<Vehicles> vehiclesList = new List<Vehicles>
+        {new Vehicles() {Id = 1, Brand= "Audi", Color="White", Vin = "12345678", Year = "1990" },
+            new Vehicles() {Id = 2, Brand= "Audi", Color="White", Vin = "12345678", Year = "1990" },
+            new Vehicles() {Id = 3, Brand= "Audi", Color="White", Vin = "12345678", Year = "1990" },
+            new Vehicles() {Id = 4, Brand= "Audi", Color="White", Vin = "12345678", Year = "1990" },
+            new Vehicles() {Id = 5, Brand= "Audi", Color="White", Vin = "12345678", Year = "1990" },
+            new Vehicles() {Id = 6, Brand= "Audi", Color="White", Vin = "12345678", Year = "1990" },
+            };
+
+            // GET: VehiclesController
+        public ActionResult Index()
+        {
+            return View(vehiclesList.OrderBy(v => v.Id).ToList());
+        }
+
+        // GET: VehiclesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VehiclesController/Create
+        public ActionResult Create()
+        {
+            var vehicleNew = new Vehicles();
+            return View(vehicleNew);
+        }
+        public ActionResult CreateVehicle(Vehicles vehicle)
+        {
+            vehiclesList.Add(vehicle);
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+
+        // GET: VehiclesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var veh = vehiclesList.Where(v => v.Id == id).FirstOrDefault();
+            return View(veh);
+        }
+
+        // POST: VehiclesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehiclesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VehiclesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
